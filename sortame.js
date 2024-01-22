fd00455828bf06fc267a2de8a176091feddb3cea
Elias Toscano <mrtoscano94@gmail.com>
2024-01-22 15:21:34 -0500

    Update on todo

---
+++ sortame.js
@@ -0,0 +1,6 @@
+// ! sort: Sorts the elements of an array in place and returns the sorted array.
+// * Mutative array methis
+let myRandomArr = [5000,21,52,36,95,4,2,5,1,25,0,15,26,48,59,62,51] 
+console.log(myRandomArr.sort((a,b) =>a - b))
+console.log(myRandomArr.sort())
+myRandomArr.sort()
