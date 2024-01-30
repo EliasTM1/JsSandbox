f0e2c5c21907e4724017197022ba382cff7f8010
Elias Toscano <mrtoscano94@gmail.com>
2024-01-29 21:16:44 -0500

    eight out of nine

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var TimeLimitedCache = function () {
-	this.myObjnow = {};
-	this.countKeys = 0;
-	this.initialDuration;
-	this.timer;
+var TimeLimitedCache = function() {
+    this.myObjnow = {}
+    this.countKeys = 0
+    this.timer
+    this.initialDuration
 };
 
-/**
+/** 
  * @param {number} key
  * @param {number} value
  * @param {number} duration time until expiration in ms
  * @return {boolean} if un-expired key already existed
  */
-TimeLimitedCache.prototype.set = function (key, value, duration) {
+TimeLimitedCache.prototype.set = function(key, value, duration) {    
 	let keepItDry = () => {
 		if (this.countKeys > 0) this.countKeys--;
 		delete this.myObjnow[key];
@@ -20,62 +20,43 @@ TimeLimitedCache.prototype.set = function (key, value, duration) {
 
 	// * If the object dont exist set it
 	if (!this.myObjnow[key]) {
-		this.initialDuration = duration;
+		this.initialDuration = duration
 		this.timer = setTimeout(keepItDry, this.initialDuration);
 		this.myObjnow[key] = value;
 		this.countKeys++;
 		return false;
 	}
+
 	if (this.myObjnow[key]) {
 		clearTimeout(this.timer);
 		setTimeout(keepItDry, duration);
 		this.myObjnow[key] = value;
 		return true;
 	}
+	
 };
 
-/**
+/** 
  * @param {number} key
  * @return {number} value associated with key
  */
-TimeLimitedCache.prototype.get = function (key) {
+TimeLimitedCache.prototype.get = function(key) {
 	if (this.myObjnow[key]) {
 		return this.myObjnow[key];
-	} else return -1;
+	}
+	return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+       return this.countKeys;
 };
 
 /**
- * @return {number} count of non-expired keys
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
  */
-TimeLimitedCache.prototype.count = function () {
-	return this.countKeys;
-};
-
-let times = [40, 800, 850];
-
-const timeLimitedCache = new TimeLimitedCache();
-setTimeout(() => {
-	timeLimitedCache.set(1, 13, 50);
-	timeLimitedCache.set(2, 14, 300);
-}, 0);
-setTimeout(() => {
-	timeLimitedCache.set(1, 15, 100);
-}, 40);
-setTimeout(() => {
-	timeLimitedCache.get(2);
-}, 800);
-setTimeout(() => {
-	timeLimitedCache.count();
-}, 850);
-
-// let numerObj = {
-// 	2: 5,
-// 	3: 8,
-// 	4: 9,
-// 	6: 7,
-// };
-
-// if (numerObj[2]) {
-// 	console.log(numerObj[2]);
-// }
-// console.log(numerObj[2]);
