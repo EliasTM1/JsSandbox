cef32a28c708bc790bf2ddb368cd581ba2651b6a
Elias Toscano <mrtoscano94@gmail.com>
2024-06-02 18:39:13 -0400

    Update

--- archive-leetCode.js
+++ archive-leetCode.js
@@ -1312,3 +1312,111 @@ useMemoize();
 //     {"id": 2, "x": 9},
 //     {"id": 3, "x": 5}
 // ]
+
+
+
+// ? Flat arrays
+
+// Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
+
+// A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
+
+// A flattened array is a version of that array with some or all of the sub-arrays removed and replaced with the actual elements in that sub-array. This flattening operation should only be done if the current depth of nesting is less than n. The depth of the elements in the first array are considered to be 0.
+
+// Please solve it without the built-in Array.flat method.
+
+// Example 1:
+
+// Input
+// let arrExamOne = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+// let nExamOne = 0
+// Output
+// [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+
+// Explanation
+// Passing a depth of n=0 will always result in the original array. This is because the smallest possible depth of a subarray (0) is not less than n=0. Thus, no subarray should be flattened.
+// Example 2:
+
+// Input
+// let arrExamTwo = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+// let nExamTwo = 1
+// Output
+// [1, 2, 3, 4, 5, 6, 7, 8, [9, 10, 11], 12, 13, 14, 15]
+
+// Explanation
+// The subarrays starting with 4, 7, and 13 are all flattened. This is because their depth of 0 is less than 1. However [9, 10, 11] remains unflattened because its depth is 1.
+// Example 3:
+
+// Input
+// let arrExamThree = [[1, 2, 3], [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+// let nExamThree = 2
+// Output
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+// Explanation
+// The maximum depth of any subarray is 1. Thus, all of them are flattened.
+
+// var flat = function (arr, n) {
+// 	let resultObj = {}
+// 	if(n === 0) {
+// 		return arr
+// 	}
+
+// 	for(let i = 0; i < arr.length; i++) {
+// 		resultObj[i] = arr[i]
+// 	}
+
+// 	for(let j = 0; j < arr.length; j++) {
+// 		if(Array.isArray(resultObj[j])) {
+// 			let currentArr = resultObj[j]
+// 			// resultObj[j] =
+// 			// for(let k = 0; k < currentArr.length; k++){
+// 			// }
+// 		}
+// 	}
+// 	// else {
+
+// 	// }
+
+// };
+
+// flat(arrExamOne, nExamOne)
+// flat(arrExamTwo, nExamTwo);
+// flat(arrExamThree, nExamThree)
+
+// * Compact
+// var compactObject = function (obj) {
+
+// let resultArr = [];
+// let bucketObj = {};
+
+// if (Array.isArray(obj)) {
+// 	for (let i = 0; i < obj.length; i++) {
+// 		bucketObj[i] = obj[i];
+// 	}
+
+// 	for (let key in bucketObj) {
+// 		if (!bucketObj[key]) {
+// 			delete bucketObj[key];
+// 		} else if (Array.isArray(bucketObj[key])) {
+// 			bucketObj[key].filter((e, index) => {
+// 				if (!e) {
+// 					return bucketObj[key].splice(index, 1);
+// 				}
+// 			});
+// 		}
+// 	}
+// 	return Object.values(bucketObj);
+// } else {
+// 	for (key in obj) {
+// 		if (!obj[key]) {
+// 			delete obj[key];
+// 		} else if (Array.isArray(obj[key])) {
+// 			obj[key] = obj[key].filter((e, index) => {
+// 				if (e) return obj[key];
+// 			});
+// 		}
+// 	}
+// 	return obj;
+// }
+// };
